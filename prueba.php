4ae75114cbff17d3b3216767cdc61de6a9dfc556
ravd45 <r.velazqez@hotmail.com>
2019-07-29 08:19:31 -0500

    actualizacion

--- prueba.php
+++ prueba.php
@@ -1,81 +1,88 @@
 <?php 
 
-$s = '06/10/2011 19:00:02';
-$date = strtotime($s);
-echo date('d/m/y H:i:s', $date);
+// $s = '06/10/2011 19:00:02';
+// $date = strtotime($s);
+// echo date('d/m/y H:i:s', $date);
 
-$fi ='10-04-2019 00:00:00';
-$ff ='10-08-2019 00:00:00';
-$h = '04-04-2019 00:00:00';
-$fecha_inicial = strtotime($fi);
-$fecha_final = strtotime($ff);
-$hoy = strtotime($h);
-$porcentaje = 67 ;
+// $fi ='10-04-2019 00:00:00';
+// $ff ='10-08-2019 00:00:00';
+// $h = '04-04-2019 00:00:00';
+// $fecha_inicial = strtotime($fi);
+// $fecha_final = strtotime($ff);
+// $hoy = strtotime($h);
+// $porcentaje = 40 ;
 
 
-//$difd = date_diff($fecha_final, $fecha_inicial);
-$difd = 122;
-echo  $difd.' diferencia de horas<br>';
-$total_horas = $difd * 24;
-echo  $total_horas.' total horas<br>';
-$horas_restantes = $total_horas / 3;
-echo $horas_restantes.' horas restantes<br>';
-$dif_hoy = 122;
-//$dif_hoy = date_diff($fecha_final, $hoy);
-echo $dif_hoy .' diferencia hoy <br>';
-$horas_restantes_hoy = $dif_hoy * 24;
-echo $horas_restantes_hoy .' dias restantes desde hoy';
+// //$difd = date_diff($fecha_final, $fecha_inicial);
+// $difd = 25;
+// echo  $difd.' diferencia de horas<br>';
+// $total_horas = $difd * 24;
+// echo  $total_horas.' total horas<br>';
+// $horas_restantes = $total_horas / 3;
+// echo $horas_restantes.' horas restantes<br>';
+// $dif_hoy = 15;
+// //$dif_hoy = date_diff($fecha_final, $hoy);
+// echo $dif_hoy .' diferencia hoy <br>';
+// $horas_restantes_hoy = $dif_hoy * 24;
+// echo $horas_restantes_hoy .' dias restantes desde hoy';
 
-if ($horas_restantes_hoy >= 0) {
+// if ($horas_restantes_hoy >= 0) {
 
-if ($horas_restantes >= $horas_restantes_hoy) {
-	echo '<br>if 1';
-	switch ($porcentaje) {
-		case ($porcentaje <= 66):
-			echo '<p style="color:red;">Rojo</p>';
-			break;
-		case ($porcentaje >= 67 && $porcentaje <= 89):
-			echo '<p style="color:yellow;">Amarillo</p>';
-			break;
-		case ($porcentaje >= 90 && $porcentaje <= 100):
-			echo '<p style="color:green;">Verde</p>';
-			break;
-			}
-}else{
-	if (($horas_restantes * 2 )>= $horas_restantes_hoy) {
-		echo '<br>if 2';
-		switch ($porcentaje) {
-		case ($porcentaje <= 33):
-			echo '<p style="color:red;">Rojo</p>';
-			break;
-		case ($porcentaje >= 33 && $porcentaje <= 66):
-			echo '<p style="color:yellow;">Amarillo</p>';
-			break;
-		case ($porcentaje >= 67 && $porcentaje <= 100):
-			echo '<p style="color:green;">Verde</p>';
-			break;
-		}
-	}else{
-		if (($horas_restantes * 3 )>= $horas_restantes_hoy) {
-			echo '<br>if 3';
-			switch ($porcentaje) {
-		case ($porcentaje >= 0 && $porcentaje <= 33):
-			echo '<p style="color:red;">Rojo</p>';
-			break;
-		case ($porcentaje >= 34 && $porcentaje <= 66):
-			echo '<p style="color:yellow;">Amarillo</p>';
-			break;
-		case ($porcentaje >= 67 && $porcentaje <= 100):
-			echo '<p style="color:green;">Verde</p>';
-			break;
-	}
-		}else{
-			echo '<br>aún no empieza el plazo';
-		}	
-	}
-}
-} else{
-	echo 'aún no empieza el plazo';
-}
+// if ($horas_restantes >= $horas_restantes_hoy) {
+// 	echo '<br>if 1';
+// 	switch ($porcentaje) {
+// 		case ($porcentaje <= 66):
+// 			echo '<p style="color:red;">Rojo</p>';
+// 			break;
+// 		case ($porcentaje >= 67 && $porcentaje <= 89):
+// 			echo '<p style="color:yellow;">Amarillo</p>';
+// 			break;
+// 		case ($porcentaje >= 90 && $porcentaje <= 100):
+// 			echo '<p style="color:green;">Verde</p>';
+// 			break;
+// 			}
+// }else{
+// 	if (($horas_restantes * 2 )>= $horas_restantes_hoy) {
+// 		echo '<br>if 2';
+// 		switch ($porcentaje) {
+// 		case ($porcentaje <= 33):
+// 			echo '<p style="color:red;">Rojo</p>';
+// 			break;
+// 		case ($porcentaje >= 33 && $porcentaje <= 66):
+// 			echo '<p style="color:yellow;">Amarillo</p>';
+// 			break;
+// 		case ($porcentaje >= 67 && $porcentaje <= 100):
+// 			echo '<p style="color:green;">Verde</p>';
+// 			break;
+// 		}
+// 	}else{
+// 		if (($horas_restantes * 3 )>= $horas_restantes_hoy) {
+// 			echo '<br>if 3';
+// 			switch ($porcentaje) {
+// 		case ($porcentaje >= 0 && $porcentaje <= 33):
+// 			echo '<p style="color:green;">Rojo</p>';
+// 			break;
+// 		case ($porcentaje >= 34 && $porcentaje <= 66):
+// 			echo '<p style="color:green;">Amarillo</p>';
+// 			break;
+// 		case ($porcentaje >= 67 && $porcentaje <= 100):
+// 			echo '<p style="color:green;">Verde</p>';
+// 			break;
+// 	}
+// 		}else{
+// 			echo '<br>aún no empieza el plazo';
+// 		}	
+// 	}
+// }
+// } else{
+// 	echo 'aún no empieza el plazo';
+// }
+
+
+$base_url = 'http://qual-edu.org/levantamiento_de_requerimientos/index.php';
+
+$nueva = substr($base_url, 0,52);
+echo $nueva;
+
 ?>
 
