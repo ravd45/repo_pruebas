9d18693e25583e6f54d68d5ea5ced9c41422f6cf
ravd45 <r.velazqez@hotmail.com>
2019-12-04 15:13:47 -0600

    asd

--- forflojera.php
+++ forflojera.php
@@ -6,10 +6,49 @@ $ids = ["insert into planeaxesc_reactivo (id_ct, id_reactivo, n_almn_eval, n_aci
 
 
 foreach ($ids as $key => $value) {
-   for ($i=0; $i < 61500 ; $i++) { 
+   for ($i=0; $i < 6 ; $i++) { 
        echo $value;
    }
 }
 
 
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <title>Document</title>
+ </head>
+ <body>
+    <style> 
+    progress {
+  border: none;
+  width: 400px;
+  height: 60px;
+  background: crimson;
+}
+
+progress {
+  color: lightblue;
+}
+
+progress::-webkit-progress-value {
+  background: lightblue;
+}
+
+progress::-moz-progress-bar {
+  background: lightcolor;
+}
+
+progress::-webkit-progress-value {
+  background: red;
+}
+
+progress::-webkit-progress-bar {
+  background: blue;
+}
+    </style>
+     <progress max="100" value="75"></progress>
+ </body>
+ </html>
